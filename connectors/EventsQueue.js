af9c2e75ea97e870c9444da8de55bcbed2ac89df
Greg JEFTIC <ajkula@gmail.com>
2019-06-12 23:28:25 +0200

    moved distance calculator method

--- connectors/EventsQueue.js
+++ connectors/EventsQueue.js
@@ -6,17 +6,6 @@ module.exports = class EventsQueue {
     this.Q = async.queue(callback);
   }
 
-  distance(source, compare) {
-    let lat1 = source.lat, lon1 = source.lon, lat2 = compare.lat, lon2 = compare.lon;
-    var p = 0.017453292519943295;    // Math.PI / 180
-    var c = Math.cos;
-    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
-            c(lat1 * p) * c(lat2 * p) * 
-            (1 - c((lon2 - lon1) * p))/2;
-  
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-  }
-
   push(events = []) {
     this.Q.push(events, err => err ? logule.error(err) : null);
   }
--- controllers/EventsAggregationController.js
+++ controllers/EventsAggregationController.js
@@ -40,8 +40,20 @@ module.exports = class EventsAggregationController extends Abstract {
 			})
 		}
 		
+
+		distance(source, compare) {
+			let lat1 = source.lat, lon1 = source.lon, lat2 = compare.lat, lon2 = compare.lon;
+			var p = 0.017453292519943295;    // Math.PI / 180
+			var c = Math.cos;
+			var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+							c(lat1 * p) * c(lat2 * p) * 
+							(1 - c((lon2 - lon1) * p))/2;
+		
+			return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+		}
+		
 		eachEvent(event, callback) {
-			let distancesFromEventArray = this.poi.map(point => this.queue.distance(event, point));
+			let distancesFromEventArray = this.poi.map(point => this.distance(event, point));
 			let current = this.poi[distancesFromEventArray.indexOf(Math.min(...distancesFromEventArray))];
 			if (!(current || {}).name) console.log({
 				current,
